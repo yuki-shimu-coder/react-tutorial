08abf716f09603000a84119c64cc7df13f772031
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-03-17 17:22:30 +0900

    ゲームの仮完成

--- src/index.js
+++ src/index.js
@@ -40,9 +40,11 @@ class Board extends React.Component {
     // var arr2 = arr1.slice(0, arr1.length);
     // 引数なしでも同じことができる
     // var arr3 = arr1.slice();
-
-    // これはstate.squaresの配列をコピーしている。
+    // これはstate.squaresの配列をコピーしている。    
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -60,7 +62,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares); 
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -107,3 +115,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
